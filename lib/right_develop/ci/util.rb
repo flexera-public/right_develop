27febdeb922118d858bf83dbe417b4b81d6baad6
Tony Spataro <tony@rightscale.com>
2014-03-24 17:30:43 -0700

    acu158421 escape '.' character in test case names so Jenkins doesn't scramble the output

--- lib/right_develop/ci/util.rb
+++ lib/right_develop/ci/util.rb
@@ -6,6 +6,14 @@ module RightDevelop::CI
     # in Java class names.
     JAVA_CLASS_NAME = /[A-Za-z0-9_]/
 
+    # The dot character gets special treatment: even if we XML-escape it, Jenkins will assume
+    # that it functions as a package separator. So, we'll replace it with an equivalent Unicode
+    # character. Hooray homographic character attacks!
+    JAVA_PACKAGE_SEPARATOR = '.'
+
+    # Replacement codepoint that looks a bit like a period
+    JAVE_PACKAGE_SEPARATOR_HOMOGLYPH = '&#xb7;'
+
     # Make a string suitable for parsing by Jenkins JUnit display plugin by escaping any non-valid
     # Java class name characters as an XML entity. This prevents Jenkins from interpreting "hi1.2"
     # as a package-and-class name.
@@ -18,6 +26,8 @@ module RightDevelop::CI
       name.each_char do |chr|
         if chr =~ JAVA_CLASS_NAME
           result << chr
+        elsif chr == JAVA_PACKAGE_SEPARATOR
+          result << JAVE_PACKAGE_SEPARATOR_HOMOGLYPH
         else
           chr = chr.unpack('U')[0].to_s(16)
           result << "&#x#{chr};"
